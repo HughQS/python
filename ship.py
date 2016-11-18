db2b435f4f2ae95ef5c19222cad09be5108f5398
HughQS <sq2483@163.com>
2016-11-18 23:17:50 +0800

    add level,displaying the number of ships

--- ship.py
+++ ship.py
@@ -4,7 +4,8 @@ from pygame.sprite import Sprite
 class Ship(Sprite):
 
     def __init__(self, ai_settings, screen):
-        """Initialize the ship and set its starting position."""
+        """Initialize the ship and set its starting    ss position."""
+
         super(Ship,self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
