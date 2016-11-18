93954a6d6075f38cc9d45b2974c40de666a72b5b
HughQS <sq2483@163.com>
2016-11-18 23:23:55 +0800

    del "ss" in ship.py

--- ship.py
+++ ship.py
@@ -4,7 +4,7 @@ from pygame.sprite import Sprite
 class Ship(Sprite):
 
     def __init__(self, ai_settings, screen):
-        """Initialize the ship and set its starting    ss position."""
+        """Initialize the ship and set its starting position."""
 
         super(Ship,self).__init__()
         self.screen = screen
